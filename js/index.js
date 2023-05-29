5e9dd2e6b19c592ba0c28f3c4ff18e9cbb37dc2b
Ryu Juheon <saidbysolo@gmail.com>
2023-05-29 02:03:30 +0000

    feat: 집에가고싶어요

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const clock = document.getElementsByClassName("RealTimeClock");
+
+const DDay = new Date("2024-09-13");
+
+const calculate = () => {
+    const now = new Date()
+    const gap = DDay.getTime() - now.getTime()
+
+    const day = Math.floor(gap / (1000 * 60 * 60 * 24))
+    const hours = Math.floor((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    const minutes = Math.floor((gap % (1000 * 60 * 60)) / (1000 * 60))
+    const seconds = Math.floor((gap % (1000 * 60)) / 1000)
+
+    return [day, hours, minutes, seconds]
+}
+
+const render = () => {
+    const [day, hours, minutes, seconds] = calculate();
+
+    clock[0].innerHTML = `그가 집에 가기까지 ${day}일 ${hours}시간 ${minutes}분 ${seconds}초 남았습니다.`;
+}
+
+setInterval(render, 1000);
